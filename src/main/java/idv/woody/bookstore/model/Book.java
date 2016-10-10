75d048e7ddde8398988d78d68a057c7b2c3530cd
woody0927 <woody1a014>
2016-10-11 02:18:48 +0800

    ComparisonChain

--- src/main/java/idv/woody/bookstore/model/Book.java
+++ src/main/java/idv/woody/bookstore/model/Book.java
@@ -1,6 +1,7 @@
 package idv.woody.bookstore.model;
 
 import com.google.common.collect.ComparisonChain;
+import com.google.common.collect.Ordering;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
@@ -43,7 +44,19 @@ public class Book implements Comparable<Book> {
     @Override
     public int compareTo(Book book) {
         return ComparisonChain.start().
-                compare(this.getPublisher(), book.getPublisher()).
+                compare(this.getPublisher(), book.getPublisher(), Ordering.natural().nullsLast()).
                 compare(this.getPrice(), book.getPrice()).result();
     }
+
+//    @Override
+//    public int compareTo(Book book) {
+//        int i = this.getPublisher() == null ?
+//                (book.getPublisher() == null ? 0 : Integer.MIN_VALUE) :
+//                (book.getPublisher() == null ? Integer.MAX_VALUE : this.getPublisher().compareTo(book.getPublisher()));
+//        if (i != 0) {
+//            return i;
+//        }
+//
+//        return Integer.compare(this.getPrice(), book.getPrice());
+//    }
 }
